bf49ca0cc75c7ca834de0348f2db946cbd3ea602
DavidG <DavidG555555@outlook.com>
2025-03-05 10:47:57 -0800

    modified:   server/config/db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 // const mongoURI = 'mongodb+srv://crudapp:crudapp@cluster0.iydyt0r.mongodb.net/?retryWrites=true&w=majority';
-// const mongoURI = 'mongodb+srv://mz7731346:kGdWOtV7Wto1kkxI@cluster0.r7mtc.mongodb.net/myfirstdatabase?retryWrites=true&w=majority&appName=Cluster0';
-const mongoURI = 'mongodb+srv://davidg555555:DgWKIJap9IcebIlB@mycluster.taxyn.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster';
+const mongoURI = 'mongodb+srv://mz7731346:kGdWOtV7Wto1kkxI@cluster0.r7mtc.mongodb.net/myfirstdatabase?retryWrites=true&w=majority&appName=Cluster0';
+// const mongoURI = 'mongodb+srv://davidg555555:DgWKIJap9IcebIlB@mycluster.taxyn.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster';
 
 const connectDB = async () => {
   try {
